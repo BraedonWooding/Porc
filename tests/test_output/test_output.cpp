d8592fee7473ec1114fae1f8c9308305bb197e37
braedonww@gmail.com <braedonww@gmail.com>
2019-08-14 10:48:29 +1000

    Updated tests + changed cmake to automatically add tests

--- tests/test_output/test_output.cpp
+++ tests/test_output/test_output.cpp
@@ -9,22 +9,17 @@
 
 using namespace porc;
 
-SCENARIO( "Constants should be loaded and output correctly", "[parser - Constant]" ) {
-  GIVEN( "A loaded sample" ) {
-      err::PipeOutput(std::cerr);
-      WHEN ( "Some simple cases" ) {
-        std::string input = "10;\ntrue;\n\"Hello\";\n";
-        TokenStream stream(std::make_unique<StringReader>(input));
-        stream.ignore_comments = true;
-        Parser parser = Parser(std::move(stream));
-        auto top_level = parser.ParseFileDecl();
-        std::stringstream ss;
-
-        THEN( "the output should match the input (relatively so)" ) {
-          REQUIRE(top_level);
-          ss << *top_level.value();
-          REQUIRE(ss.str() == input);
-        }
-      }
+TEST_CASE( "Constants are outputted correctly", "[dev-print ast]" ) {
+  err::PipeOutput(std::cerr);
+  SECTION ( "Some simple cases" ) {
+    std::string input = "10;\ntrue;\n\"Hello\";\n";
+    TokenStream stream(std::make_unique<StringReader>(input));
+    stream.ignore_comments = true;
+    Parser parser = Parser(std::move(stream));
+    auto top_level = parser.ParseFileDecl();
+    REQUIRE(top_level);
+    std::stringstream ss;
+    ss << *top_level.value();
+    REQUIRE(ss.str() == input);
   }
 }
